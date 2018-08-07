0425a05684c2811583ac4b296dd264b15c9eb8d2
DESKTOP-NVG3H6F\jar10 <jar10038@gmail.com>
2018-08-07 17:34:56 +0900

    계산기 구현하기 calculor 는 gui로 구현중

---
+++ calcujs.js
@@ -0,0 +1,11 @@
+// 메인함수
+function main() {
+
+}
+var clickNums = function(event) {
+    console.log(event.target.innerHTML);
+}
+
+var clickOthers = function(event) {
+    console.log(event.target.innerHTML);
+}
