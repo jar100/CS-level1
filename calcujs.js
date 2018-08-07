0def0323b1d144be382f41421256f0d05ac40ce1
DESKTOP-NVG3H6F\jar10 <jar10038@gmail.com>
2018-08-07 20:09:59 +0900

    계산기 수정버전 입력하는것도 출력하게했다

--- calcujs.js
+++ calcujs.js
@@ -3,10 +3,15 @@ function main() {
 }
 let str = [''];
 let count = 0;
+let str2 = '';
+let screen1 = document.getElementById('output');
+let screen2 = document.getElementById('screen');
 
 var clickNums = function(event) {
     str[count] += event.target.innerHTML;
     console.log(event.target.innerHTML);
+    str2 += event.target.innerHTML;
+    screen2.innerHTML = str2;
 }
 
 var clickOthers = function(event) {
@@ -15,33 +20,25 @@ var clickOthers = function(event) {
         str.push('');
         count++;
         str[count] += event.target.innerHTML;
+        str2 += event.target.innerHTML;
         console.log(event.target.innerHTML);
+        screen2.innerHTML = str2;
         str.push('');
         count++;
     } else if (event.target.innerHTML = 'BS') {
         str = [''];
         count = 0;
-        var screen = document.getElementById('output');
-        screen.innerHTML ='0';
+        screen1.innerHTML ='답: 0';
+        screen2.innerHTML = '';
     }
 
 }
 
 
-// 연산자  개수
 
-var findStr = function(input) {
-    let count1 = 0 ;
-    for (var i = 0 ; i < input.length; i++) {
-        if(input[i] === '+' || input[i] === '-' || input[i] === '/' || input[i] === '*' ) {
-            conut1++;
-        } else if (input[i] === '=') {
-            count1++;
-        }
-    }
-    return count1;
-}
-
+// 계산기 동작
+// 4가지 케이스에 따라 동작하게 만듬
+// 답도 출력하게 만듬
 var calcu = function(input) {
     var reselt =0;
     var screen = document.getElementById('output');
@@ -63,10 +60,10 @@ var calcu = function(input) {
             break;
         }
     }
-    screen.innerHTML ='답: '+reselt;
+    screen1.innerHTML ='답: '+reselt;
 }
 
-
+//배열 값중 숫자는 숫자로 변경 사실 필요없는듯?
 var changeInput = function(input) {
     for(var i = 0; i < input.length; i++) {
         if(input[i] === '+' || input[i] === '-' || input[i] === '/' || input[i] === '*' || input[i] === '=' ) {
