fc17f243953b974a74896474c7a6c71e6332b9c8
DESKTOP-NVG3H6F\jar10 <jar10038@gmail.com>
2018-08-21 23:19:09 +0900

    소수 구하기 에라토스테네스의 체를 이용해서 구현

--- prim.js
+++ prim.js
@@ -1,8 +1,26 @@
-var input = 10;
-
-var primeArr =[];
-for (i = 0; i< input; i++) {
-    primeArr.push(1);
-
+var a = solution(10);
+function solution(n) {
+    var answer = 0;
+    var primArr = [0,0];
+    var primArr2 = [];
+    for ( var i = 2; i <= n ; i++ ) {
+        primArr[i]= i;
+    }
+    for (var i = 2; i <= Math.sqrt(n) ; i ++) {
+        if (primArr[i] === 0) {
+            continue;
+        }
+        for (var j = i+i; j<= n; j +=i )  {
+            primArr[j] = 0;
+        }
+        
+    }
+    for (var i =0; i < primArr.length; i ++ ) {
+        if( primArr[i] !== 0) {
+            primArr2.push(primArr[i]);
+        }
+    }
+    console.log(primArr2);
+    answer = primArr2.length;
+    return answer;
 }
-console.log(primeArr);
